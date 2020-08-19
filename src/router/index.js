239302fd42cbe5ec8e5d10a411beaa3616319136
toonoisy <toonoisy@outlook.com>
2020-08-19 19:39:38 +0800

    fix: 修复编程式导航replace抛错的bug

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import routes from '@/router/routes';
 Vue.use(VueRouter); // 声明使用插件（vue官方/自定义插件都需要声明使用）
 
 const originPush = VueRouter.prototype.push; // 保存原始push函数，后面用的时候还能找到
+const originReplace = VueRouter.prototype.replace;
 
 VueRouter.prototype.push = function(location, onResolved, onRejected) {
   // 没有处理promise回调
@@ -14,6 +15,13 @@ VueRouter.prototype.push = function(location, onResolved, onRejected) {
   // 处理了
   return originPush.call(this, location, onResolved, onRejected);
 }; // 只要出现{}，就是一个新的对象
+
+VueRouter.prototype.replace = function(location, onResolved, onRejected) {
+  if (!onResolved && !onRejected) {
+    return originReplace.call(this, location).catch(() => {});
+  };
+  return originReplace.call(this, location, onResolved, onRejected);
+};
 
 // 向外暴露一个路由器对象(管理多个路由)
 const router = new VueRouter({
